63619c4b539622826b5da3c4ce8bbd54e1b469df
mitchellgkm <mitchell.gkm@gmail.com>
2020-11-22 21:47:34 +0000

    removed jquery and updated calc CSS

--- news.js
+++ news.js
@@ -1,12 +1,17 @@
-newscardsection = $("#newscardsection");
+newscardsection = document.getElementById("newscardsection");
 
-newsbutton = $("#newsbutton");
+newscardsection.style.visibility = "hidden";
 
-$(newscardsection).hide();
+newsbutton = document.getElementById("newsbutton");
+
+newsbutton.addEventListener("click", (event) => {
+  newscardsection.style.visibility = "visible";
+  getNews();
+});
 
 function getNews() {
   fetch(
-    "https://gnews.io/api/v4/top-headlines?q=technology&lang=en&token=e06bf95933475a42fa3991b29f5cd00e"
+    "https://gnews.io/api/v4/top-headlines?q=technology&lang=en&country=us&token=e06bf95933475a42fa3991b29f5cd00e"
   ) //GET request to Gnews server
     .then(function (response) {
       //Convert the response into a JSON object
@@ -15,19 +20,18 @@ function getNews() {
     .then(function (data) {
       //Access the JSON object
       for (var i = 0; i < 8; i++) {
-        $(`#newsimg${i}`).attr("src", data.articles[i].image); //Insert each object's article image
+        document
+          .getElementById(`newsimg${i}`)
+          .setAttribute("src", data.articles[i].image); //Insert each object's article image
 
-        $(`#newstitle${i}`).text(data.articles[i].title); //Insert each title
+        document.getElementById(`newstitle${i}`).innerText =
+          data.articles[i].title; //Insert each title
 
-        $(`#newstitle${i}`).attr("href", data.articles[i].url); //Insert url into title / image
+        document
+          .getElementById(`newstitle${i}`)
+          .setAttribute("href", data.articles[i].url); //Insert url into title / image
 
         // $(`#newstext${i}`).text(data.articles[i].description); //Insert each description -> removed description
       }
     });
 }
-
-$(newsbutton).click(function (e) {
-  e.preventDefault();
-  $(newscardsection).show();
-  getNews();
-});
